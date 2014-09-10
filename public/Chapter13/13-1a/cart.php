76786f489a3d4312dfd9d5408871596f25993aa9
Josh Forbes <jdforbes82@gmail.com>
2014-09-10 13:18:14 -0400

    added 13-1a homework

---
+++ public/Chapter13/13-1a/cart.php
@@ -0,0 +1,53 @@
+<?php
+// Add an item to the cart
+function add_item($cart, $key, $quantity) {
+    global $products;
+    if ($quantity < 1) return;
+
+    // If item already exists in cart, update quantity
+    if (isset($cart[$key])) {
+        $quantity += $cart[$key]['qty'];
+        $cart = update_item($cart, $key, $quantity);
+        return $cart;
+    }
+
+    // Add item
+    $cost = $products[$key]['cost'];
+    $total = $cost * $quantity;
+    $item = array(
+        'name' => $products[$key]['name'],
+        'cost' => $cost,
+        'qty'  => $quantity,
+        'total' => $total
+    );
+    $cart[$key] = $item;
+    return $cart;
+}
+
+// Update an item in the cart
+function update_item($cart, $key, $quantity) {
+    global $products;
+    $quantity = (int) $quantity;
+    if (isset($cart[$key])) {
+        if ($quantity <= 0) {
+            unset($cart[$key]);
+        } else {
+            $cart[$key]['qty'] = $quantity;
+            $total = $cart[$key]['cost'] *
+                     $cart[$key]['qty'];
+            $cart[$key]['total'] = $total;
+        }
+    }
+    return $cart;
+}
+
+// Get cart subtotal
+function get_subtotal ($cart) {
+    $subtotal = 0;
+    foreach ($cart as $item) {
+        $subtotal += $item['total'];
+    }
+    $subtotal = number_format($subtotal, 2);
+    return $subtotal;
+}
+?>
