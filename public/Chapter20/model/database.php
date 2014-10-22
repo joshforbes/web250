b65fc90bcd5254d1d9930ca496794cd4ac18caf6
Josh Forbes <jdforbes82@gmail.com>
2014-10-21 20:22:57 -0400

    Chapter 20 homework

---
+++ public/Chapter20/model/database.php
@@ -0,0 +1,27 @@
+<?php
+
+if (isset($_SERVER['APP_ENV']) && $_SERVER['APP_ENV'] === 'local') {
+    putenv('host=localhost');
+    putenv('username=homestead');
+    putenv('password=secret');
+}
+
+$dsn = 'mysql:host=' . getenv('host') . ';dbname=my_guitar_shop2';
+$username = getenv('username');
+$password = getenv('password');
+$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+
+try {
+    $db = new PDO($dsn, $username, $password, $options);
+} catch (PDOException $e) {
+    $error_message = $e->getMessage();
+    include 'errors/db_error_connect.php';
+    exit;
+}
+
+function display_db_error($error_message) {
+    global $app_path;
+    include 'errors/db_error.php';
+    exit;
+}
+?>
