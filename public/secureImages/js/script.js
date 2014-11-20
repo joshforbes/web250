41fcfc20bd456e249b82ac2eac223794d96c6ad6
Josh Forbes <jdforbes82@gmail.com>
2014-11-19 20:08:37 -0500

    further file name revisions

--- public/secureImages/js/script.js
+++ public/secureImages/js/script.js
@@ -9,8 +9,9 @@ var uploadModule = (function() {
                 this.on("success", function(file, response) {
                     self = this;
                     s.imageContainer.prepend(s.imageTemplate);
-                    alert(file.name.toLowerCase());
-                    $('.image:first').attr('data-id', response.id).css("background-image", "url('uploads/" + file.name.toLowerCase() + "')");
+                    var fileName = file.name.toLocaleLowerCase();
+                    alert(fileName);
+                    $('.image:first').attr('data-id', response.id).css("background-image", "url('uploads/" + fileName + "')");
 
                     if (this.getUploadingFiles().length === 0 && this.getQueuedFiles().length === 0) {
                         window.setTimeout(function() { dropzoneSlideToggle(); }, 1000);
